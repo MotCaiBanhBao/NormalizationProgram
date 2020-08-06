0275efb0289bf5b9c45a17e74a86f01a8f5d5fce
luongvany <luongvany12@gmail.com>
2020-08-06 15:55:29 +0700

    Thay Doi 1 so thuat toan

--- AllSrc/src/LuocDoQuanHe.kt
+++ AllSrc/src/LuocDoQuanHe.kt
@@ -4,31 +4,31 @@ class LuocDoQuanHe(leftList: MutableList<String>, u: String, rightList: MutableL
     val right = rightList
     val functionalDependency = mutableListOf<MutableList<String>>(leftList, rightList)
 
-    fun timBaoDong(thuocTinhCanTim:String):String{
-        var conThayDoi = true
-        var baoDong = thuocTinhCanTim
-        while (conThayDoi){
-            conThayDoi = false;
-            for(temp in left)
-                if(isContains(baoDong, temp)){
-                    val thuocTinhThem = right[left.indexOf(temp)]
-                    if(!isContains(baoDong, thuocTinhThem)){
-                        baoDong = addAtribute(baoDong, thuocTinhThem)
-                        conThayDoi = true
+    private fun findClosure(source:String):String{
+        var isStillChangeAble = true
+        var closure = source
+        while (isStillChangeAble){
+            isStillChangeAble = false;
+            for(index in 0 until functionalDependency[0].size)
+                if(isContains(closure, functionalDependency[0][index])){
+                    val addAttribute = functionalDependency[1][index]
+                    if(!isContains(closure, addAttribute)){
+                        closure = addAttribute(closure, addAttribute)
+                        isStillChangeAble = true
                     }
                 }
         }
-        return baoDong
+        return closure
     }
-    private fun addAtribute(source: String, atribute: String): String{
+     private fun addAttribute(source: String, attribute: String): String{
         var result = source
-        var atributeArray = atribute.toCharArray()
-        for(oneAtribute in atributeArray)
-            if(!result.contains(oneAtribute))
-                result += oneAtribute
+        var attributeArray = attribute.toCharArray()
+        for(oneAttribute in attributeArray)
+            if(!result.contains(oneAttribute))
+                result += oneAttribute
         return result
     }
-     fun isContains(Container: String, needCheckString: String): Boolean{
+     private fun isContains(Container: String, needCheckString: String): Boolean{
         val needCheckToArray = needCheckString.toCharArray()
         for(temp in needCheckToArray)
             if(!Container.contains(temp))
@@ -47,20 +47,16 @@ class LuocDoQuanHe(leftList: MutableList<String>, u: String, rightList: MutableL
             temp++
         }
         removeDuplicate()
-        for(i in 0 until functionalDependency[0].size){
-            println("${functionalDependency[0][i]} -> ${functionalDependency[1][i]}")
-        }
     }
-    fun convertRHS(index: Int){
+    private fun convertRHS(index: Int){
         for(temp in (functionalDependency[1][index]).toCharArray()){
             functionalDependency[0].add(functionalDependency[0][index])
             functionalDependency[1].add(temp.toString())
         }
         functionalDependency[0].removeAt(index)
         functionalDependency[1].removeAt(index)
-
     }
-    fun removeDuplicate(){
+    private fun removeDuplicate(){
         var sizeOfSet = functionalDependency[0].size
         for (temp in 0 until sizeOfSet-2){
             for(temp2 in temp+1 until sizeOfSet-1){
@@ -74,23 +70,18 @@ class LuocDoQuanHe(leftList: MutableList<String>, u: String, rightList: MutableL
         }
     }
     fun findLHSExtraneous(){
-        var tempFunctionalDependency : MutableList<MutableList<String>> = mutableListOf()
-        tempFunctionalDependency.addAll(functionalDependency)
-        var sizeOfSet = tempFunctionalDependency[0].size
+        var sizeOfSet = functionalDependency[0].size
         for(temp in 0 until sizeOfSet){
-            var subString = findAllSubString(tempFunctionalDependency[0][temp])
+            var subString = findAllSubString(functionalDependency[0][temp])
             for (temp2 in subString){
-                if(isContains(timBaoDong(temp2), tempFunctionalDependency[1][temp])){
-                    tempFunctionalDependency[0][temp] = temp2
+                if(isContains(findClosure(temp2), functionalDependency[1][temp])){
+                    functionalDependency[0][temp] = temp2
                     break
                 }
             }
         }
-        for(i in 0 until sizeOfSet){
-            println("${tempFunctionalDependency[0][i]} -> ${tempFunctionalDependency[1][i]}")
-        }
     }
-    fun findAllSubString(source: String): MutableList<String>{
+    private fun findAllSubString(source: String): MutableList<String>{
         var sourceArray = source.toCharArray()
         val arrSize = source.length
         var result: MutableList<String> = mutableListOf()
@@ -109,5 +100,10 @@ class LuocDoQuanHe(leftList: MutableList<String>, u: String, rightList: MutableL
                 result.removeAt(i)
         }
         return result
+    }
+    fun output(){
+        for(i in 0 until functionalDependency[0].size){
+            println("${functionalDependency[0][i]} -> ${functionalDependency[1][i]}")
+        }
     }
 }
